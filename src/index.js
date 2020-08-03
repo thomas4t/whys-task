b36cfd1d537a22338b28e8651f3a3adc5d68cc71
Tomáš Trávníček <travnicek.t494@gmail.com>
2020-08-03 11:48:42 +0200

    many changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    lightBlue: "#61dafb",
+    darkBlue: "#0C2D48",
+    darkGrey: "#282c34",
+    babyBlue: "#BFD7ED",
+    blueGrotto: "#60A3D9",
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("APP")
 );
